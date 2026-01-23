d4b7fb98894e1337f21fe00c78b321c2ddeb2343
Septusix <jahleelnemuel@gmail.com>
2026-01-23 17:02:33 +0800

    feat: refactor loading state handling to consistently display loading indicator across Decks, Quizzes, and Reviewers pages

--- client/src/pages/ReviewersPage.jsx
+++ client/src/pages/ReviewersPage.jsx
@@ -23,7 +23,7 @@ export default function ReviewersPage() {
             return toast.error(message);
         } catch (error) {
             console.error('Error on loadAllReviewer:', error);
-        }finally{
+        } finally {
             setLoading(false);
         }
     }
@@ -33,8 +33,6 @@ export default function ReviewersPage() {
             loadAllReviewer();
         });
     }, []);
-
-    if (loading) return <Loading />;
 
     return (
         <div className="min-h-screen flex flex-col">
@@ -70,6 +68,7 @@ export default function ReviewersPage() {
                     loadAllReviewer={loadAllReviewer}
                 />
             }
+            {loading && <Loading />}
         </div>
     )
 }
